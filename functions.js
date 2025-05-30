87a0750509f910f962bd264223206fea8b4d041b
L-hub-lab <leahmoosa16@gmail.com>
2025-05-30 15:53:27 +0200

    Completed Ex6

---
+++ functions.js
@@ -0,0 +1,19 @@
+// TODO: Create a function called greet that takes a name parameter
+function greet(name) {
+    // TODO: Make the greet function return "Hello, [name]!"
+    return `Hello, ${name}!`;
+}
+
+// TODO: Create a function called add that takes two parameters
+function add(a, b) {
+    // TODO: Make the add function return the sum of both parameters
+    return a + b;
+}
+
+// TODO: Call both functions and store their results in variables
+let greeting = greet("Alice");
+let sumResult = add(5, 7);
+
+// TODO: Print the results to the console
+console.log(greeting);     // Output: Hello, Alice!
+console.log(sumResult);    // Output: 12
